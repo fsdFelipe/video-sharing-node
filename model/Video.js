e6764079928610c0aeb8347d02af422baad25f2e
fsdFelipe <devfsdfelipe@gmail.com>
2022-08-06 19:34:30 -0300

    Criando models = Usuario, video, Commnent

---
+++ model/Video.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const videoSchema = new mongoose.Schema(
+    {
+        userId:{
+            type : String,
+            required : true,
+            unique : true,
+        },
+        title:{
+            type : String,
+            required : true,
+        },
+        desc:{
+            type : String,
+            required : true,
+        },
+        imgUrl:{
+            type : String,
+            required : true,
+        },
+        videoUrl:{
+            type : String,
+            required : true,
+        },
+        views:{
+            type : Number,
+            default : 0,
+        },
+        tags:{
+            type : [String],
+            default : []
+        },
+        like:{
+            type : [String],
+            default : []
+        },
+        dislike:{
+            type : [String],
+            default : []
+        },
+    }
+    {timestamps : true}
+)
+export default mongoose.model('Videos', videoSchema)
