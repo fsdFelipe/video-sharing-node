57db17a8e4cddd2070933cd15a9d2ef96fa6b0c1
fsdFelipe <devfsdfelipe@gmail.com>
2022-08-07 11:18:42 -0300

    Route e controller de video e comments

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/usuarioRoute.js'
+import videoRoutes from './routes/videoRoute.js'
+import commentRoutes from './routes/commentRoute.js'
 
 const app = express()
 dotenv.config()
 
 app.use(express.json())
 app.use('/api/usuarios', userRoutes)
+app.use('/api/videos', videoRoutes)
+app.use('/api/comentarios', commentRoutes)
 
 const connect =()=>{
     mongoose.connect(process.env.MONGO).then(()=>{
